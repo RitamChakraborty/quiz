59617bebf04aa0ab8833ea794b702aacf27f5eff
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-03-20 22:03:01 +0530

    Experiment with container size

--- lib/v2/view/home_page.dart
+++ lib/v2/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz/v2/service/quiz_service.dart';
 
@@ -8,14 +9,25 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     List<String> categories = _quizService.categories;
 
-    Widget categoryTile({@required String title}) => Container(
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-            image:
-                Image.asset("assets/images/${title.toLowerCase()}.webp").image,
-          )),
+    Widget categoryTile({@required String title}) => Wrap(
+          alignment: WrapAlignment.center,
+          children: [
+            Card(
+              elevation: 10,
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image:
+                        Image.asset("assets/images/${title.toLowerCase()}.webp")
+                            .image,
+                  ),
+                ),
+              ),
+            ),
+          ],
         );
 
     return Material(
