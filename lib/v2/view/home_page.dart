98fc97b8ac1faf5b6eb4a849f0c160f6b5d297d2
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-03-23 20:48:51 +0530

    Create grid view to show the categories

--- lib/v2/view/home_page.dart
+++ lib/v2/view/home_page.dart
@@ -9,33 +9,45 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     List<String> categories = _quizService.categories;
 
-    Widget categoryTile({@required String title}) => Wrap(
-          alignment: WrapAlignment.center,
-          children: [
-            Card(
-              elevation: 10,
-              child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image:
-                        Image.asset("assets/images/${title.toLowerCase()}.webp")
-                            .image,
-                  ),
+    Widget categoryTile({@required String title}) => Container(
+          padding: const EdgeInsets.all(16),
+          child: Card(
+            elevation: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image:
+                      Image.asset("assets/images/${title.toLowerCase()}.webp")
+                          .image,
                 ),
               ),
             ),
-          ],
+          ),
         );
+
+    double getMaxCrossAxisExtend() {
+      MediaQueryData mediaQueryData = MediaQuery.of(context);
+      Size size = mediaQueryData.size;
+
+      if (size.height > size.width) {
+        return mediaQueryData.size.width / 1.5;
+      } else {
+        return mediaQueryData.size.height / 2.5;
+      }
+    }
 
     return Material(
       child: Container(
-        child: ListView.builder(
+        margin: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: GridView.builder(
           itemCount: categories.length,
           itemBuilder: (BuildContext context, int index) =>
               categoryTile(title: categories[index]),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: getMaxCrossAxisExtend(),
+            childAspectRatio: 1,
+          ),
         ),
       ),
     );
