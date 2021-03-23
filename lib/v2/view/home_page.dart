1e064327007be41cd8fa9afb71baa362f50c242f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-03-23 21:00:46 +0530

    Add I'm Feeling Luck floating action button

--- lib/v2/view/home_page.dart
+++ lib/v2/view/home_page.dart
@@ -9,6 +9,31 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     List<String> categories = _quizService.categories;
 
+    // Functions
+
+    /// Returns the MaxCrossAxisExtend value according to the orientation
+    double getMaxCrossAxisExtend() {
+      MediaQueryData mediaQueryData = MediaQuery.of(context);
+      Size size = mediaQueryData.size;
+
+      if (size.height > size.width) {
+        return mediaQueryData.size.width / 1.5;
+      } else {
+        return mediaQueryData.size.height / 2.5;
+      }
+    }
+
+    /// I'm Feeling Lucky button action
+    void floatingActionButtonOnPressed() {}
+
+    // Widgets
+    final floatingActionButtonLabelText = Text(
+      "I'm Feeling Lucky",
+      style: TextStyle(
+        color: Colors.blue,
+      ),
+    );
+
     Widget categoryTile({@required String title}) => Container(
           padding: const EdgeInsets.all(16),
           child: Card(
@@ -26,30 +51,28 @@ class HomePage extends StatelessWidget {
           ),
         );
 
-    double getMaxCrossAxisExtend() {
-      MediaQueryData mediaQueryData = MediaQuery.of(context);
-      Size size = mediaQueryData.size;
-
-      if (size.height > size.width) {
-        return mediaQueryData.size.width / 1.5;
-      } else {
-        return mediaQueryData.size.height / 2.5;
-      }
-    }
-
     return Material(
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: GridView.builder(
-          itemCount: categories.length,
-          itemBuilder: (BuildContext context, int index) =>
-              categoryTile(title: categories[index]),
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: getMaxCrossAxisExtend(),
-            childAspectRatio: 1,
+        child: Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        label: floatingActionButtonLabelText,
+        onPressed: floatingActionButtonOnPressed,
+        backgroundColor: Colors.white.withOpacity(0.9),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: GridView.builder(
+            itemCount: categories.length,
+            itemBuilder: (BuildContext context, int index) =>
+                categoryTile(title: categories[index]),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: getMaxCrossAxisExtend(),
+              childAspectRatio: 1,
+            ),
           ),
         ),
       ),
-    );
+    ));
   }
 }
