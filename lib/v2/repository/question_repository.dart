2075a65a16f36154ae3a95b50173865eaf66e48e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-05-01 08:02:49 +0530

    Create question_repository and question to support v1

--- lib/v2/repository/question_repository.dart
+++ lib/v2/repository/question_repository.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:quiz/v1/model/question.dart';
+import 'package:quiz/v2/mode/question.dart';
 import 'package:quiz/v2/mode/question_parameter.dart';
 
 class QuestionRepository {
