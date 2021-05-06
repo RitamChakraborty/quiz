c837c28e342bc8e8b2689dd5a86ff998c35bb8f7
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-05-06 21:59:47 +0530

    Show green to correct option and red to wrong answer

--- lib/widget/question_container.dart
+++ lib/widget/question_container.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/model/question.dart';
 
-class QuestionContainer extends StatelessWidget {
+class QuestionContainer extends StatefulWidget {
+  @override
+  _QuestionContainerState createState() => _QuestionContainerState();
+}
+
+class _QuestionContainerState extends State<QuestionContainer> {
+  final correctColor = Colors.green;
+  final wrongColor = Colors.red;
+  var color = Colors.purple;
+  var colors = [];
+  final Question question = Question(
+    type: "any",
+    difficulty: "any",
+    category: "any",
+    correctAnswer: "correct",
+    incorrectAnswers: ["incorrect1", "incorrect2", "incorrect3"],
+    question: "This is a question for which no answer exists!",
+  );
+  List<String> options = [];
+  var correctAnswerIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    options = question.options;
+    for (int i = 0; i < options.length; ++i) {
+      colors.add(color);
+
+      if (options[i] == question.correctAnswer) {
+        correctAnswerIndex = i;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Question question = Question(
-      type: "any",
-      difficulty: "any",
-      category: "any",
-      correctAnswer: "correct",
-      incorrectAnswers: ["incorrect1", "incorrect2", "incorrect3"],
-      question: "This is a question for which no answer exists!",
-    );
-
     Widget questionContainer(String question) => Expanded(
           child: Container(
             padding: const EdgeInsets.all(16),
@@ -33,13 +57,19 @@ class QuestionContainer extends StatelessWidget {
           ),
         );
 
-    Widget optionWidget(String option) => Container(
+    Widget optionWidget(int index, String option, {bool isCorrect = false}) =>
+        Container(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: Material(
             elevation: 10,
-            color: Colors.deepPurple,
+            color: colors[index],
             child: MaterialButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  colors[index] = wrongColor;
+                  colors[correctAnswerIndex] = correctColor;
+                });
+              },
               child: AnimatedContainer(
                 margin: const EdgeInsets.all(16),
                 alignment: Alignment.center,
@@ -55,8 +85,15 @@ class QuestionContainer extends StatelessWidget {
           ),
         );
 
-    List<Widget> optionWidgets =
-        question.options.map((option) => optionWidget(option)).toList();
+    List<Widget> optionWidgets = [];
+
+    for (int i = 0; i < options.length; ++i) {
+      String option = options[i];
+      var widget = option == question.correctAnswer
+          ? optionWidget(i, option, isCorrect: true)
+          : optionWidget(i, option);
+      optionWidgets.add(widget);
+    }
 
     return Material(
       child: Container(
