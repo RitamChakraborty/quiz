c37211a4f3e0e442a5cfb7c0f5e485a0874c0db6
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-05-12 19:30:16 +0530

    Navigate to start page from the score_page

--- lib/view/score_page.dart
+++ lib/view/score_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/model/Result.dart';
+import 'package:quiz/view/home_page.dart';
 
 class ScorePage extends StatelessWidget {
   static const routeName = "/scorePage";
@@ -9,9 +10,14 @@ class ScorePage extends StatelessWidget {
     final Result result = ModalRoute.of(context).settings.arguments as Result;
 
     return Material(
-      child: Container(
-        child: Center(
-          child: Text("${result.marksObtained}/${result.totalMarks}"),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pushReplacementNamed(HomePage.routeName);
+        },
+        child: Container(
+          child: Center(
+            child: Text("${result.marksObtained}/${result.totalMarks}"),
+          ),
         ),
       ),
     );
