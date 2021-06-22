472d891c974eac98313033e3ada153b521e5f213
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-06-22 19:33:19 +0530

    Failed to navigate to home_page after game ends

--- lib/view/score_page.dart
+++ lib/view/score_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/model/result.dart';
-import 'package:quiz/view/home_page.dart';
 
 class ScorePage extends StatefulWidget {
   static const routeName = "/scorePage";
@@ -46,13 +45,13 @@ class _ScorePageState extends State<ScorePage>
       ),
     );
 
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    _controller
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.dispose();
+        }
+      })
+      ..forward();
   }
 
   @override
@@ -60,102 +59,103 @@ class _ScorePageState extends State<ScorePage>
     final Result result = ModalRoute.of(context).settings.arguments as Result;
     double height = MediaQuery.of(context).size.height;
 
+    void playAgain() {
+      // Todo: Fix route
+      Navigator.of(context)
+          .popUntil(ModalRoute.withName(Navigator.defaultRouteName));
+    }
+
+    Widget marks = Text(
+      "${result.marksObtained} / ${result.totalMarks}",
+      style: TextStyle(
+        fontSize: Theme.of(context).textTheme.headline5.fontSize,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+
+    Widget comment = Text(
+      "${result.comment}",
+      style: TextStyle(
+        fontSize: Theme.of(context).textTheme.headline4.fontSize - 4,
+      ),
+    );
+
+    Widget playAgainButton = Padding(
+      padding: const EdgeInsets.only(bottom: 48.0),
+      child: InkWell(
+        onTap: playAgain,
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32,
+            vertical: 16,
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Theme.of(context).primaryColor.withOpacity(0.8),
+                Theme.of(context).accentColor.withOpacity(0.8),
+              ],
+            ),
+          ),
+          child: Text(
+            "Play Again",
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ),
+    );
+
     return Material(
       child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return SafeArea(
-              child: Stack(
-                fit: StackFit.expand,
-                children: [
-                  Column(
-                    children: [
-                      ScaleTransition(
-                        scale: _sizeAnimation,
-                        child: Container(
-                          margin: EdgeInsets.only(
-                            top: _marginAnimation.value * height * 0.25,
-                          ),
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          height: 150,
-                          width: 150,
-                          child: FittedBox(
-                            child: Text(
-                                "${result.marksObtained} / ${result.totalMarks}",
-                                style: TextStyle(
-                                  fontSize: Theme.of(context)
-                                      .textTheme
-                                      .headline5
-                                      .fontSize,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
+        animation: _controller,
+        builder: (_, child) {
+          return SafeArea(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Column(
+                  children: [
+                    ScaleTransition(
+                      scale: _sizeAnimation,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          top: _marginAnimation.value * height * 0.25,
+                        ),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        height: 150,
+                        width: 150,
+                        child: FittedBox(
+                          child: marks,
                         ),
                       ),
-                    ],
-                  ),
-                  FadeTransition(
-                    opacity: _opacityAnimation,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "${result.comment}",
-                          style: TextStyle(
-                            fontSize:
-                                Theme.of(context).textTheme.headline4.fontSize -
-                                    4,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 80,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 48.0),
-                          child: MaterialButton(
-                            onPressed: () {
-                              Navigator.of(context)
-                                  .pushReplacementNamed(HomePage.routeName);
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 32,
-                                vertical: 16,
-                              ),
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                  begin: Alignment.centerLeft,
-                                  end: Alignment.bottomRight,
-                                  colors: [
-                                    Theme.of(context)
-                                        .primaryColor
-                                        .withOpacity(0.8),
-                                    Theme.of(context)
-                                        .accentColor
-                                        .withOpacity(0.8),
-                                  ],
-                                ),
-                              ),
-                              child: Text(
-                                "Play Again",
-                                style: Theme.of(context).textTheme.headline6,
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
                     ),
-                  )
-                ],
-              ),
-            );
-          }),
+                  ],
+                ),
+                FadeTransition(
+                  opacity: _opacityAnimation,
+                  child: child,
+                ),
+              ],
+            ),
+          );
+        },
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            comment,
+            SizedBox(height: 80),
+            playAgainButton,
+          ],
+        ),
+      ),
     );
   }
 }
