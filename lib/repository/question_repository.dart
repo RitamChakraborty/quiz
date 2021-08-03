23cf43c082ef760fc63487177619a59ff18a225f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-08-03 09:14:00 +0530

    Add isolates to run json parsing

--- lib/repository/question_repository.dart
+++ lib/repository/question_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:quiz/model/question.dart';
 import 'package:quiz/model/question_parameter.dart';
@@ -7,11 +8,12 @@ import 'package:quiz/model/question_parameter.dart';
 class QuestionRepository {
   final http.Client _client = http.Client();
 
-  Future<List<Question>> getQuestions(QuizParameter quizParameter) async {
+  /// Parse Json response to a List<[Question]>
+  ///
+  /// @param body Response body of the api
+  /// @return List of [Question]
+  List<Question> _getQuestions(String body) {
     final List<Question> questions = [];
-    final http.Response response =
-        await _client.get(Uri.parse(quizParameter.toString()));
-    final String body = response.body;
     final json = jsonDecode(body);
 
     if (json['response_code'] == 0) {
@@ -23,5 +25,15 @@ class QuestionRepository {
     }
 
     return questions;
+  }
+
+  /// Fetch questions from the api and parse it in an isolate
+  ///
+  /// @param quizParameter [QuizParameter] gives the api url
+  /// @returns List of [Question] as Future
+  Future<List<Question>> getQuestions(QuizParameter quizParameter) async {
+    final http.Response response =
+        await _client.get(Uri.parse(quizParameter.toString()));
+    return compute(_getQuestions, response.body);
   }
 }
